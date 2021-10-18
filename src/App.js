b96eec03515a3a29111edcfa1a5ff354bf33d96c
mjc695 <jenningtan21@gmail.com>
2021-10-18 19:12:05 -0400

    added firebase and pathing for routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import logo from './logo.svg';
 
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 
 import Homepage from './pages/Homepage';
 import './App.css';
 import Navbar from './components/Navbar';
+import AboutPage from './pages/Aboutpage';
+import Blogpage from './pages/Blogpage'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Switch>
-        <Route path='/'>{Homepage}</Route>
+        <Route path='/about' component={AboutPage} />
+        <Route path='/' component={Homepage} />
+        <Route path='/blog' component={Blogpage} />
+        {/* <Redirect from='/:' to ='/'></Redirect> */}
       </Switch>
     </div>
   );
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import './Homepage.css'
+
 const Homepage = () => {
     return(
-        <div>
-            <h1>HOMEPAGE FOR CATERINA :)</h1>
+        <div className='homepage-container'>
+            <h1 className='homepage-title'>Welcome to Caterina's Website :)</h1>
+            <img className = 'main-picture' src={process.env.PUBLIC_URL +'/homepage_picture.jpg'} alt=''/>
+            <div className='introduction'>
+                Hello, this is where Caterina will welcome and explain her website to people! 
+                Learn about all of the plants here :)
+            </div>
         </div>
     )
 }
